979129cffd172fcfa65067273c05c7a4d576cfae
Dominik <dompollok@gmail.com>
2024-05-27 00:13:18 +0200

    Added Import

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ from urllib.parse import quote
 
 from flask import Flask, jsonify
 from pymongo import MongoClient
+
 from model.personal_information import PersonalInfo
+from model.role import UserRole
 from model.user import User
 from db import initialize_db, check_db_connection
 
