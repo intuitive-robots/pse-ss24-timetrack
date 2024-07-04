39797b05b13895af73b1356b509b3907cb915154
Phil <phil.gengenbach@online.de>
2024-07-04 19:37:39 +0200

    Fix secretary routing mapping

---
+++ frontend/src/pages/analysis-page/SecretaryAnalysisPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SecretaryAnalysisPage: React.FC = () => {
+    return (
+        <div className="px-6 py-6">
+            <h1 className="text-3xl font-bold text-gray-800 mt-5">Analysis</h1>
+
+        </div>
+    );
+};
+
+export default SecretaryAnalysisPage;
