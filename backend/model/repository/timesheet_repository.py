6c978d2b75889b52a3b35b43a1ca7ab708133674
DP11212 <dompollok@gmail.com>
2024-07-07 12:22:12 +0200

    Integrated Exception Handling in repo classes

--- backend/model/repository/timesheet_repository.py
+++ backend/model/repository/timesheet_repository.py
@@ -6,6 +6,8 @@ from db import initialize_db
 from model.request_result import RequestResult
 from model.timesheet import Timesheet
 from model.timesheet_status import TimesheetStatus
+from pymongo.errors import PyMongoError
+
 
 
 class TimesheetRepository:
@@ -42,10 +44,12 @@ class TimesheetRepository:
         """
         if timesheet_id is None:
             return None
-        timesheet_data = self.db.timesheets.find_one({"_id": ObjectId(timesheet_id)})
-        if timesheet_data:
-            return timesheet_data
-        return None
+        try:
+            timesheet_data = self.db.timesheets.find_one({"_id": ObjectId(timesheet_id)})
+            if timesheet_data:
+                return timesheet_data
+        except PyMongoError as e:
+            return None
 
     def get_timesheet(self, username: str, month: int, year: int):
         """
@@ -58,10 +62,12 @@ class TimesheetRepository:
         """
         if username is None or month is None or year is None:
             return None
-        timesheet_data = self.db.timesheets.find_one({"username": username, "month": month, "year": year})
-        if timesheet_data:
-            return timesheet_data
-        return None
+        try:
+            timesheet_data = self.db.timesheets.find_one({"username": username, "month": month, "year": year})
+            if timesheet_data:
+                return timesheet_data
+        except PyMongoError as e:
+            return None
 
     def get_current_timesheet(self, username: str):
         """
@@ -72,11 +78,12 @@ class TimesheetRepository:
         """
         if username is None:
             return None
-        timesheet_data = self.db.timesheets.find_one({"username": username, "status": TimesheetStatus.NOT_SUBMITTED.value})
-
-        if timesheet_data:
-            return timesheet_data
-        return None
+        try:
+            timesheet_data = self.db.timesheets.find_one({"username": username, "status": TimesheetStatus.NOT_SUBMITTED.value})
+            if timesheet_data:
+                return timesheet_data
+        except PyMongoError as e:
+            return None
 
     def get_timesheets_by_time_period(self, username:str, start_date: date, end_date: date):
         """
@@ -93,14 +100,18 @@ class TimesheetRepository:
         start_year = start_date.year
         end_month = end_date.month
         end_year = end_date.year
-        timesheet_data = self.db.timesheets.find({"username": username,
-                                                  "$or": [
-                                                      {"year": {"$gt": start_year, "$lt": end_year}},
-                                                      {"$and": [{"year": start_year}, {"year": {"$lt": end_year}}], "month": {"$gte": start_month}},
-                                                      {"$and": [{"year": end_year}, {"year": {"$gt": start_year}}], "month": {"$lte": end_month}},
-                                                      {"$and": [{"year": end_year}, {"year": start_year}, {"month": {"$gte": start_month}}, {"month": {"$lte": end_month}}]}
-                                                  ]})
-        return list(timesheet_data)
+        try:
+            timesheet_data = self.db.timesheets.find({"username": username,
+                                                      "$or": [
+                                                          {"year": {"$gt": start_year, "$lt": end_year}},
+                                                          {"$and": [{"year": start_year}, {"year": {"$lt": end_year}}], "month": {"$gte": start_month}},
+                                                          {"$and": [{"year": end_year}, {"year": {"$gt": start_year}}], "month": {"$lte": end_month}},
+                                                          {"$and": [{"year": end_year}, {"year": start_year}, {"month": {"$gte": start_month}}, {"month": {"$lte": end_month}}]}
+                                                      ]})
+            return list(timesheet_data)
+        except PyMongoError as e:
+            return None
+
 
     def get_timesheets(self):
         """
@@ -108,8 +119,12 @@ class TimesheetRepository:
         
         :return: A list of all Timesheet objects
         """
-        timesheets = self.db.timesheets.find()
-        return list(timesheets)
+        try:
+            timesheets = self.db.timesheets.find()
+            return list(timesheets)
+        except PyMongoError as e:
+            return None
+
 
     def get_timesheets_by_status(self, status: TimesheetStatus):
         """
@@ -120,8 +135,11 @@ class TimesheetRepository:
         """
         if status is None:
             return None
-        timesheets = self.db.timesheets.find({"status": str(status)})
-        return list(timesheets)
+        try:
+            timesheets = self.db.timesheets.find({"status": str(status)})
+            return list(timesheets)
+        except PyMongoError as e:
+            return None
 
     def get_timesheet_id(self, username: str, month: int, year: int):
         """
@@ -134,10 +152,12 @@ class TimesheetRepository:
         """
         if username is None or month is None or year is None:
             return None
-        timesheet_data = self.db.timesheets.find_one({"username": username, "month": month, "year": year})
-        if timesheet_data:
-            return timesheet_data['_id']
-        return None
+        try:
+            timesheet_data = self.db.timesheets.find_one({"username": username, "month": month, "year": year})
+            if timesheet_data:
+                return timesheet_data['_id']
+        except PyMongoError as e:
+            return None
 
     def get_timesheets_by_username_status(self, username: str, status: TimesheetStatus):
         """
@@ -149,8 +169,12 @@ class TimesheetRepository:
         """
         if username is None or status is None:
             return None
-        timesheets = self.db.timesheets.find({"username": username, "status": str(status)})
-        return list(timesheets)
+        try:
+            timesheets = self.db.timesheets.find({"username": username, "status": str(status)})
+            return list(timesheets)
+        except PyMongoError as e:
+            return None
+
 
     def get_timesheets_by_username(self, username: str):
         """
@@ -161,8 +185,11 @@ class TimesheetRepository:
         """
         if username is None:
             return None
-        timesheets = self.db.timesheets.find({"username": username})
-        return list(timesheets)
+        try:
+            timesheets = self.db.timesheets.find({"username": username})
+            return list(timesheets)
+        except PyMongoError as e:
+            return None
 
     def update_timesheet_by_dict(self, timesheet_data: dict) -> RequestResult:
         """
@@ -173,14 +200,17 @@ class TimesheetRepository:
         """
         if timesheet_data is None:
             return RequestResult(False, "Please provide a timesheet to update.", 400)
-        result = self.db.timesheets.update_one({"_id": ObjectId(timesheet_data['_id'])},
-                                               {"$set": timesheet_data})
-        if result.matched_count == 0:
-            return RequestResult(False, "Timesheet not found", 404)
-        if result.modified_count == 0:
-            return RequestResult(False, "Timesheet update failed", 500)
-        if result.acknowledged:
-            return RequestResult(True, "Timesheet updated successfully", 200)
+        try:
+            result = self.db.timesheets.update_one({"_id": ObjectId(timesheet_data['_id'])},
+                                                   {"$set": timesheet_data})
+            if result.matched_count == 0:
+                return RequestResult(False, "Timesheet not found", 404)
+            if result.modified_count == 0:
+                return RequestResult(False, "Timesheet update failed", 500)
+            if result.acknowledged:
+                return RequestResult(True, "Timesheet updated successfully", 200)
+        except PyMongoError as e:
+            return RequestResult(False, f"Timesheet update failed: {str(e)}", 500)
         return RequestResult(False, "Timesheet update failed", 500)
 
     def update_timesheet(self, timesheet) -> RequestResult:
@@ -192,14 +222,17 @@ class TimesheetRepository:
         """
         if timesheet is None:
             return RequestResult(False, "Please provide a timesheet to update.", 400)
-        result = self.db.timesheets.update_one({"_id": ObjectId(timesheet.timesheet_id)},
+        try:
+            result = self.db.timesheets.update_one({"_id": ObjectId(timesheet.timesheet_id)},
                                                {"$set": timesheet.to_dict()})
-        if result.matched_count == 0:
-            return RequestResult(False, "Timesheet not found", 404)
-        if result.modified_count == 0:
-            return RequestResult(False, "Timesheet update failed", 500)
-        if result.acknowledged:
-            return RequestResult(True, "Timesheet updated successfully", 200)
+            if result.matched_count == 0:
+                return RequestResult(False, "Timesheet not found", 404)
+            if result.modified_count == 0:
+                return RequestResult(False, "Timesheet update failed", 500)
+            if result.acknowledged:
+                return RequestResult(True, "Timesheet updated successfully", 200)
+        except PyMongoError as e:
+            return RequestResult(False, f"Timesheet update failed: {str(e)}", 500)
         return RequestResult(False, "Timesheet update failed", 500)
 
     def set_timesheet_status(self, timesheet_id: str, status) -> RequestResult:
@@ -212,13 +245,16 @@ class TimesheetRepository:
         """
         if timesheet_id is None or status is None:
             return RequestResult(False, "Please provide a timesheet ID and status to update the timesheet status.", 400)
-        result = self.db.timesheets.update_one({"_id": ObjectId(timesheet_id)}, {"$set": {"status": str(status)}})
-        if result.matched_count == 0:
-            return RequestResult(False, "Timesheet not found", 404)
-        if result.modified_count == 0:
-            return RequestResult(False, "Timesheet status update failed", 500)
-        if result.acknowledged:
-            return RequestResult(True, "Timesheet status updated successfully", 200)
+        try:
+            result = self.db.timesheets.update_one({"_id": ObjectId(timesheet_id)}, {"$set": {"status": str(status)}})
+            if result.matched_count == 0:
+                return RequestResult(False, "Timesheet not found", 404)
+            if result.modified_count == 0:
+                return RequestResult(False, "Timesheet status update failed", 500)
+            if result.acknowledged:
+                return RequestResult(True, "Timesheet status updated successfully", 200)
+        except PyMongoError as e:
+            return RequestResult(False, f"Timesheet status update failed: {str(e)}", 500)
         return RequestResult(False, "Timesheet status update failed", 500)
 
     def create_timesheet_by_dict(self, timesheet_data: dict):
@@ -230,14 +266,17 @@ class TimesheetRepository:
         """
         if timesheet_data is None:
             return RequestResult(False, "Please provide a timesheet to create.", 400)
-        if self.get_timesheet(timesheet_data['username'], timesheet_data['month'], timesheet_data['year']) is not None:
-            return RequestResult(False, "Timesheet already exists", 409)
-        if '_id' in timesheet_data:
-            del timesheet_data['_id']
-        result = self.db.timesheets.insert_one(timesheet_data)
-        if result.acknowledged:
-            return RequestResult(True, f'Timesheet created successfully with ID: {str(result.inserted_id)}', 201,
-                                 data={"_id": result.inserted_id})
+        try:
+            if self.get_timesheet(timesheet_data['username'], timesheet_data['month'], timesheet_data['year']) is not None:
+                return RequestResult(False, "Timesheet already exists", 409)
+            if '_id' in timesheet_data:
+                del timesheet_data['_id']
+            result = self.db.timesheets.insert_one(timesheet_data)
+            if result.acknowledged:
+                return RequestResult(True, f'Timesheet created successfully with ID: {str(result.inserted_id)}', 201,
+                                     data={"_id": result.inserted_id})
+        except PyMongoError as e:
+            return RequestResult(False, f"Timesheet creation failed: {str(e)}", 500)
         return RequestResult(False, "Timesheet creation failed", 500)
 
     def create_timesheet(self, timesheet: Timesheet):
@@ -249,13 +288,16 @@ class TimesheetRepository:
         """
         if timesheet is None:
             return RequestResult(False, "Please provide a timesheet to create.", 400)
-        if self.get_timesheet(timesheet.username, timesheet.month, timesheet.year) is not None:
-            return RequestResult(False, "Timesheet already exists", 409)
-        timesheet_data = timesheet.to_dict()
-        result = self.db.timesheets.insert_one(timesheet_data)
-        if result.acknowledged:
-            return RequestResult(True, f'Timesheet created successfully with ID: {str(result.inserted_id)}', 201,
-                                 data={"_id": result.inserted_id})
+        try:
+            if self.get_timesheet(timesheet.username, timesheet.month, timesheet.year) is not None:
+                return RequestResult(False, "Timesheet already exists", 409)
+            timesheet_data = timesheet.to_dict()
+            result = self.db.timesheets.insert_one(timesheet_data)
+            if result.acknowledged:
+                return RequestResult(True, f'Timesheet created successfully with ID: {str(result.inserted_id)}', 201,
+                                     data={"_id": result.inserted_id})
+        except PyMongoError as e:
+            return RequestResult(False, f"Timesheet creation failed: {str(e)}", 500)
         return RequestResult(False, "Timesheet creation failed", 500)
 
     def delete_timesheet(self, timesheet_id) -> RequestResult:
@@ -267,11 +309,14 @@ class TimesheetRepository:
         """
         if timesheet_id is None:
             return RequestResult(False, "Please provide a timesheet ID to delete the timesheet.", 400)
-        result = self.db.timesheets.delete_one({"_id": ObjectId(timesheet_id)})
-        if result.deleted_count == 0:
-            return RequestResult(False, "Timesheet deletion failed", 500)
-        if result.acknowledged:
-            return RequestResult(True, "Timesheet deleted successfully", 200)
+        try:
+            result = self.db.timesheets.delete_one({"_id": ObjectId(timesheet_id)})
+            if result.deleted_count == 0:
+                return RequestResult(False, "Timesheet deletion failed", 500)
+            if result.acknowledged:
+                return RequestResult(True, "Timesheet deleted successfully", 200)
+        except PyMongoError as e:
+            return RequestResult(False, f"Timesheet deletion failed: {str(e)}", 500)
         return RequestResult(False, "Timesheet deletion failed", 500)
 
 
