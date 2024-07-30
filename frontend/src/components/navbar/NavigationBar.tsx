737166933b23681d2d74ca5bc9ec09f04c49e14e
Phil <phil.gengenbach@online.de>
2024-07-31 01:51:40 +0200

    Deactive mobile navbar

--- frontend/src/components/navbar/NavigationBar.tsx
+++ frontend/src/components/navbar/NavigationBar.tsx
@@ -5,7 +5,6 @@ import navigationIcons from "./NavigationIcons";
 import {isValidRole} from "../auth/roles";
 import {buttonConfigurations} from "./ActionButtonsConfig";
 import PopupActionButton from "../input/PopupActionButton";
-import { FaBars, FaTimes } from "react-icons/fa";
 import RightArrow from "../../assets/images/arrow_right.svg";
 
 
@@ -81,70 +80,68 @@ const NavigationBar: React.FC = (): React.ReactElement => {
     return typeof icon === 'string' ? <img src={icon} alt={altText} className="mr-3" /> : <span className="mr-3">{icon}</span>;
   };
 
-  return (
-    <div className="flex flex-col w-full lg:w-72 h-full shadow-navbar-shadow border-r-2.7 border-border-gray transition-all duration-200 ease-in-out">
-      {/* Mobile Navbar Toggle */}
-      <div className="lg:hidden flex items-center justify-between p-4">
-        {/*<div className="text-xl font-semibold">Logo</div>*/}
-        <button
-            className="p-1.5 mr-8 rounded-md bg-neutral-100 border-[1.4px] border-[#eee] hover:bg-neutral-200 z-50"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-          <div className={`w-6 h-6 transform transition-transform duration-200 ${isMobileMenuOpen ? 'rotate-90' : 'rotate-0'}`}>
-            <img src={RightArrow} alt="RightArrow" className="w-6 h-6"/>
-          </div>
-        </button>
-      </div>
+  // return (
+//     <div className="flex flex-col w-full lg:w-72 h-full shadow-navbar-shadow border-r-2.7 border-border-gray transition-all duration-200 ease-in-out">
+//       {/* Mobile Navbar Toggle */}
+//       <div className="lg:hidden flex items-center justify-between p-4">
+//         {/*<div className="text-xl font-semibold">Logo</div>*/}
+//         <button
+//             className="p-1.5 mr-8 rounded-md bg-neutral-100 border-[1.4px] border-[#eee] hover:bg-neutral-200 z-50"
+//             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+//           <div className={`w-6 h-6 transform transition-transform duration-200 ${isMobileMenuOpen ? 'rotate-90' : 'rotate-0'}`}>
+//             <img src={RightArrow} alt="RightArrow" className="w-6 h-6"/>
+//           </div>
+//         </button>
+//       </div>
+//
+//       {/* Navigation Items */}
+//       <div
+//           className={`h-full py-14 px-6 gap-6 flex flex-col font-semibold ${isMobileMenuOpen ? 'block' : 'hidden'} lg:block`}>
+//         {Object.keys(menuItems).map((section) => (
+//             <div key={section}>
+//               <div className="flex text-md tracking-wide text-[#BFBFBF] mb-2">
+//               {section}
+//               </div>
+//             {menuItems[section].map((item) => (
+//               <button
+//                 key={item}
+//                 className={`flex items-center text-left w-full py-2 px-4 rounded-md
+//                   ${
+//                     activeItem === item
+//                       ? "bg-navbar-selected-bg text-gray-800 py-2 transition duration-300 ease-in-out transform"
+//                       : "text-gray-600 hover:bg-navbar-selected-bg"
+//                   }`}
+//                 onClick={() => handleItemClick(item)}
+//               >
+//                 {renderIcon(activeItem === item ? navigationIcons[item.replace(/\s+/g, '')].active : navigationIcons[item.replace(/\s+/g, '')].default, `${item} icon`)}
+//                 {item}
+//               </button>
+//             ))}
+//           </div>
+//         ))}
+//         <div className="mt-auto">
+//           {buttons.length > 0 && (
+//             <PopupActionButton
+//               icon={buttons[0].icon}
+//               label={buttons[0].label}
+//               popupComponent={buttons[0].popup}
+//               primary={true}
+//             />
+//           )}
+//           {buttons.length > 1 && (
+//             <PopupActionButton
+//               icon={buttons[1].icon}
+//               label={buttons[1].label}
+//               popupComponent={buttons[1].popup}
+//               secondary={true}
+//             />
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-      {/* Navigation Items */}
-      <div
-          className={`h-full py-14 px-6 gap-6 flex flex-col font-semibold ${isMobileMenuOpen ? 'block' : 'hidden'} lg:block`}>
-        {Object.keys(menuItems).map((section) => (
-            <div key={section}>
-              <div className="flex text-md tracking-wide text-[#BFBFBF] mb-2">
-              {section}
-              </div>
-            {menuItems[section].map((item) => (
-              <button
-                key={item}
-                className={`flex items-center text-left w-full py-2 px-4 rounded-md
-                  ${
-                    activeItem === item
-                      ? "bg-navbar-selected-bg text-gray-800 py-2 transition duration-300 ease-in-out transform"
-                      : "text-gray-600 hover:bg-navbar-selected-bg"
-                  }`}
-                onClick={() => handleItemClick(item)}
-              >
-                <img
-                  src={activeItem === item ? navigationIcons[item.replace(/\s+/, '')].active : navigationIcons[item.replace(/\s+/, '')].default}
-                  className="mr-3 fill-amber-200" alt={`${item} icon`} />
-                {item}
-              </button>
-            ))}
-          </div>
-        ))}
-        <div className="mt-auto">
-          {buttons.length > 0 && (
-            <PopupActionButton
-              icon={buttons[0].icon}
-              label={buttons[0].label}
-              popupComponent={buttons[0].popup}
-              primary={true}
-            />
-          )}
-          {buttons.length > 1 && (
-            <PopupActionButton
-              icon={buttons[1].icon}
-              label={buttons[1].label}
-              popupComponent={buttons[1].popup}
-              secondary={true}
-            />
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-  /*
   return (
     <div className="flex-col w-72 h-full shadow-navbar-shadow border-r-2.7 border-border-gray transition-all duration-200 ease-in-out">
       <div className="h-full py-14 px-6 gap-6 flex flex-col font-semibold">
@@ -193,6 +190,6 @@ const NavigationBar: React.FC = (): React.ReactElement => {
   );
 };
 
-   */
+
 
 export default NavigationBar;
