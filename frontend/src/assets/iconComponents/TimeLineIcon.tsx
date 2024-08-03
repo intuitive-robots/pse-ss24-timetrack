64df754f58853a03c78c4752c92728c07a08ca35
Phil <phil.gengenbach@online.de>
2024-08-03 22:59:59 +0200

    Connect to  contractual hiwi data endpoint

--- frontend/src/assets/iconComponents/TimeLineIcon.tsx
+++ frontend/src/assets/iconComponents/TimeLineIcon.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 export const TimeLineIcon = () => {
     return (
         <svg width="4" height="584" viewBox="0 9 4 584" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2 0L2 584" stroke="url(#paint0_linear_237_2668)" stroke-opacity="0.22" stroke-width="3.2"/>
+            <path d="M2 0L2 584" stroke="url(#paint0_linear_237_2668)" strokeOpacity="0.22" strokeWidth="3.2"/>
             <defs>
                 <linearGradient id="paint0_linear_237_2668" x1="2.5" y1="-2.18557e-08" x2="2.50003" y2="584"
                                 gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#444444"/>
-                    <stop offset="0.83" stop-color="#999999"/>
-                    <stop offset="1" stop-color="#AAAAAA" stop-opacity="0"/>
+                    <stop stopColor="#444444"/>
+                    <stop offset="0.83" stopColor="#999999"/>
+                    <stop offset="1" stopColor="#AAAAAA" stopOpacity="0"/>
                 </linearGradient>
             </defs>
         </svg>
