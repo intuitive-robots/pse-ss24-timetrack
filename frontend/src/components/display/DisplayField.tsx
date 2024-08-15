7c6bd971fe2ffa04ed9ef1ed879c3a98dbcff0ae
Johann Kohl <uclje@student.kit.edu>
2024-08-15 19:37:15 +0200

    change user view popup

--- frontend/src/components/display/DisplayField.tsx
+++ frontend/src/components/display/DisplayField.tsx
@@ -10,10 +10,14 @@ interface DisplayFieldProps {
 const DisplayField: React.FC<DisplayFieldProps> = ({ label, value, icon }) => {
   return (
     <div className="input-container">
-      <h2 className="text-md font-semibold mb-1.5">{label}</h2>
+      <h2 className="text-md font-semibold text-[#494949] mb-1.5">{label}</h2>
       <div className="input-wrapper flex items-center border border-gray-300 rounded-md overflow-hidden px-4 gap-1 w-56">
-        {icon}
-        <div className="input-field flex-1 outline-none p-2 text-gray-700">{value}</div>
+        <div className="flex-shrink-0">
+          {icon}
+        </div>
+        <div className="input-field flex-1 outline-none p-2 text-gray-700 truncate">
+          {value}
+        </div>
       </div>
     </div>
   );
