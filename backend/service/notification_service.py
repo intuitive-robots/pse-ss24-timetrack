4746af5154738b8899687244bd660dc45c4bda3e
DP11212 <dompollok@gmail.com>
2024-08-22 00:56:38 +0200

    Fixed Notification Controller Tests

--- backend/service/notification_service.py
+++ backend/service/notification_service.py
@@ -83,7 +83,7 @@ class NotificationService:
             for notification in new_notifications:
                 if not notification.read:
                     notification.read = True
-                    self.notification_repository.update_notification(notification)
+                    update_result = self.notification_repository.update_notification(notification)
             read_result.data = sorted(read_result.data, key=lambda x: x.timestamp, reverse=True)
             return RequestResult(True, "Notifications retrieved successfully", 200, data=read_result.data)
         return read_result
--- backend/tests/controller/test_notification_controller.py
+++ backend/tests/controller/test_notification_controller.py
@@ -1,20 +1,82 @@
 import unittest
 
 from app import app
+from db import initialize_db
 from model.notification.message_type import MessageType
 from model.notification.notification_message import NotificationMessage
 from model.repository.notification_repository import NotificationRepository
+from model.repository.user_repository import UserRepository
+from model.user.hiwi import Hiwi
+from model.user.user import User
 from service.notification_service import NotificationService
+from utils.security_utils import SecurityUtils
 
 
 class TestNotificationController(unittest.TestCase):
-    def setUp(self):
-        self.app = app
-        self.client = app.test_client()
-        self.notification_repository = NotificationRepository.get_instance()
-        self.notification_service = NotificationService()
 
-    def authenticate(self, username, password):
+    def setUp(self):
+        self.notification = NotificationMessage(
+            sender="AdminNotificationController",
+            receiver="HiwiNotificationController",
+            message="Testing",
+            message_type=MessageType.REMINDER,
+        )
+    @classmethod
+    def setUpClass(cls):
+        cls.app = app
+        cls.client = app.test_client()
+        cls.db = initialize_db()
+        cls.notification_repository = NotificationRepository.get_instance()
+        cls.notification_service = NotificationService()
+        slack_user_id = "U07BENARPHB"  # Replace this with your own slack user id
+
+        cls.admin_user_data = {
+            "username": "AdminNotificationController",
+            "role": "Admin",
+            "passwordHash": SecurityUtils.hash_password("test_password"),
+            "personalInfo": {
+                "firstName": "Test",
+                "lastName": "Admin",
+                "email": "test@gmail.com",
+                "personalNumber": "6381212",
+                "instituteName": "Info Institute"
+            },
+            "accountCreation": None,
+            "lastLogin": None,
+            "isArchived": False,
+            "slackId": slack_user_id
+        }
+        admin_user = User.from_dict(cls.admin_user_data)
+        user_repository = UserRepository.get_instance()
+        user_repository.create_user(admin_user)
+        cls.hiwi_user_data = {
+            "username": "HiwiNotificationController",
+            "passwordHash": SecurityUtils.hash_password("test_password"),
+            "role": "Hiwi",
+            "personalInfo": {
+                "firstName": "Test",
+                "lastName": "LastName",
+                "email": "test@gmail.com",
+                "personalNumber": "6381212",
+                "instituteName": "Info Institute"
+            },
+            "contractInfo": {
+                "hourlyWage": 12.40,
+                "workingHours": 80
+            },
+            "supervisor": "testSupervisorUserService",
+            "slackId": slack_user_id,
+            "accountCreation": None,
+            "lastLogin": None
+        }
+        hiwi_user = Hiwi.from_dict(cls.hiwi_user_data)
+        user_repository.create_user(hiwi_user)
+
+
+    def tearDown(self):
+        self.db.notifications.delete_many({"message": "Testing"})
+
+    def _authenticate(self, username, password):
         """
         Authenticate the user.
         """
@@ -27,57 +89,44 @@ class TestNotificationController(unittest.TestCase):
 
     def test_does_unread_messages_exist(self):
         with self.app.app_context():
-            access_token = self.authenticate('testHiwi1', 'test_password')
+            access_token = self._authenticate('HiwiNotificationController', 'test_password')
             with self.app.test_request_context(headers={"Authorization": f"Bearer {access_token}"}):
-                notification = NotificationMessage(
-                    sender="testHiwi2",
-                    receiver="testHiwi1",
-                    message="Test message",
-                    message_type=MessageType.REMINDER,
-                )
-                read_result = self.notification_service.read_all_notifications()
-                creation_result = self.notification_repository.create_notification(notification)
+
+                creation_result = self.notification_repository.create_notification(self.notification)
                 response = self.client.get('/notification/doesUnreadMessageExist',
                                            headers={"Authorization": f"Bearer {access_token}"})
-                self.notification_repository.delete_notification_by_id(creation_result.data.get("id"))
                 self.assertEqual(response.status_code, 200)
                 self.assertEqual(True, response.json)
-
+    def test_does_unread_messages_exist_no_unread_messages(self):
+        with self.app.app_context():
+            access_token = self._authenticate('HiwiNotificationController', 'test_password')
+            with self.app.test_request_context(headers={"Authorization": f"Bearer {access_token}"}):
                 response = self.client.get('/notification/doesUnreadMessageExist',
                                            headers={"Authorization": f"Bearer {access_token}"})
                 self.assertEqual(response.status_code, 200)
                 self.assertEqual(False, response.json)
 
+
     def test_read_all_notifications(self):
         with self.app.app_context():
-            access_token = self.authenticate('testHiwi1', 'test_password')
+            access_token = self._authenticate('HiwiNotificationController', 'test_password')
             with self.app.test_request_context(headers={"Authorization": f"Bearer {access_token}"}):
-                notification = NotificationMessage(
-                    sender="testHiwi2",
-                    receiver="testHiwi1",
-                    message="Test message",
-                    message_type=MessageType.REMINDER,
-                )
-                creation_result = self.notification_repository.create_notification(notification)
+
+                creation_result = self.notification_repository.create_notification(self.notification)
                 response = self.client.get('/notification/readAll',
                                            headers={"Authorization": f"Bearer {access_token}"})
-                self.notification_repository.delete_notification_by_id(creation_result.data.get("id"))
                 self.assertEqual(response.status_code, 200)
                 does_unread_message_exist_response = self.client.get('/notification/doesUnreadMessageExist',
                                                                     headers={"Authorization": f"Bearer {access_token}"})
                 self.assertEqual(does_unread_message_exist_response.status_code, 200)
                 self.assertEqual(False, does_unread_message_exist_response.json)
+
     def test_delete_notification(self):
         with self.app.app_context():
-            access_token = self.authenticate('testHiwi1', 'test_password')
+            access_token = self._authenticate('HiwiNotificationController', 'test_password')
             with self.app.test_request_context(headers={"Authorization": f"Bearer {access_token}"}):
-                notification = NotificationMessage(
-                    sender="testHiwi2",
-                    receiver="testHiwi1",
-                    message="Test message",
-                    message_type=MessageType.REMINDER,
-                )
-                creation_result = self.notification_repository.create_notification(notification)
+
+                creation_result = self.notification_repository.create_notification(self.notification)
                 response = self.client.delete('/notification/delete?id=' + creation_result.data.get("id"),
                                               headers={"Authorization": f"Bearer {access_token}"})
                 self.assertEqual(response.status_code, 200)
