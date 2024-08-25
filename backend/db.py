842578908994e30bb42791edd3b6b56f0e25f195
Dominik Pollok <dompollok@gmail.com>
2024-08-25 14:40:20 +0200

    Corrected DB

--- backend/db.py
+++ backend/db.py
@@ -12,6 +12,6 @@ def initialize_db():
     global client
 
     client = MongoClient('158.180.40.137', 27017, username='admin', password='TimeTracking123!')
-    db = client.timetracking_db_final_tests
+    db = client.timetracking_db
     return db
 
