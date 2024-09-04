3c113b0271579834e59562659e3a9306fe8f9017
Dominik Pollok <dompollok@gmail.com>
2024-09-04 20:30:26 +0200

    DB Pwd and Username now env vars

--- backend/db.py
+++ backend/db.py
@@ -14,7 +14,9 @@ def initialize_db():
     global client
 
     db_host = os.getenv('DB_HOST', 'localhost') # Get the DB_HOST environment variable or use 'localhost' as default
-    client = MongoClient(db_host, 27017, username='admin', password='TimeTracking123!')
+    db_username = os.getenv('DB_USERNAME', 'admin') # Get the DB_USERNAME environment variable or use 'admin' as default
+    db_password = os.getenv('DB_PASSWORD', 'TimeTracking123!') # Get the DB_PASSWORD environment variable or use 'TimeTracking123!' as default
+    client = MongoClient(db_host, 27017, username=db_username, password=db_password) # Connect to the MongoDB server
     db = client.timetracking_db_production
 
     return db
