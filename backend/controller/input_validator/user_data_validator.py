eaffc937dd97ab6a52dbcc57fb195115eec40c3c
Maximilian Li <maximilian.xiling.li@gmail.com>
2025-02-07 09:46:14 +0100

    make email optional

--- backend/controller/input_validator/user_data_validator.py
+++ backend/controller/input_validator/user_data_validator.py
@@ -22,7 +22,7 @@ class UserDataValidator(InputValidator):
             'password': r'.{8,}',  # At least 8 characters
             'firstName': r'^[\p{L}]{2,15}$',  # Unicode letters, 2-15 characters long
             'lastName': r'^[\p{L}]{2,20}$',  # Unicode letters, 2-20 characters long
-            'email': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+            #'email': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
             'personalNumber': r'^\d{4,10}$',  # 4 to 10 digits
             'instituteName': r'^[a-zA-Z\s]{2,40}$'  # Only letters and spaces, 2-20 characters long
         }
